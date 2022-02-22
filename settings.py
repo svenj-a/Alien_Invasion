4fe2bf0f2b4a297399dbb357b4d98e212f740a5c
Matrix <svenja.faust@fechten-schleswig.de>
2022-02-22 22:46:52 +0100

    Created files settings, ship, alien_invasion
    Created a module for settings, a module for ship and a code file with the main loop for alien invasion.

---
+++ settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 18:52:46 2022
+
+@author: Matrix
+"""
+
+#%%
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen esttings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
